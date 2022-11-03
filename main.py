523a47d151d781dd3cf6f6dff14a7235e8d32f84
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2022-11-03 14:27:22 +1300

    generated n number of bottom arcs - equidistant apart

--- main.py
+++ main.py
@@ -13,29 +13,31 @@ app = Ursina()
 window.color = color.black
 
 # create a sphere
-sphere_scale = tuple(i*5 for i in (1,1,1))
+radius = 5 # scale factor
+sphere_scale = tuple(i*radius for i in (1,1,1))
 sphere = Entity(model='sphere', color=color.black, scale=sphere_scale, position=(0,0,0), dz=1e+35)
-# square = Entity(model='quad', color=color.red, scale=sphere_scale, position=(0,0,0))
 # create photon ring
 circle = Entity(model='circle', color=color.rgba(243,216,192,100), scale=tuple(i+0.05 for i in sphere_scale), position=(0,0,0))
-# add lines
-# line = Entity(model='line', color=color.orange, scale=(8,8), position=(0,-3))
-# generate vertices
-vertices = []
-for y in np.linspace(0,5):
-    x = np.sqrt(25-(y**2))
-    # positive square root answer
-    vertices.append([x, y, 0])
-
-# add negative square root answers
-
-for i in reversed(vertices.copy()):
-    vertices.append([i[j]*-1 if j==0 else i[j] for j in range(len(i))])
-# vertices.append([[i[j]*-1 if j==0 else i[j] for j in range(len(i))] for i in vertices])
-# vertices[-1] = vertices[-1][0]
-# convert to a tuple
-vertices = tuple([tuple(i) for i in vertices])
-line = Entity(model=Mesh(vertices=vertices, mode='line'), color=color.orange, scale=1)
+# add bottom arcs/black hole's underside
+bottomArcs = []
+arcRadius = radius
+# create 10 arcs
+for numArcs in range(50):
+    # generates points
+    vertices = []
+    # loop through a set of possible y values of a circle
+    for y in np.linspace(0, -arcRadius):
+        x = np.sqrt((arcRadius**2) - (y)**2)
+        # positive square root answer
+        vertices.append([x, y, 0])
+    # add negative square root answers
+    for i in reversed(vertices.copy()):
+        vertices.append([i[j]*-1 if j==0 else i[j] for j in range(len(i))])
+    # convert to a tuple
+    vertices = tuple([tuple(i) for i in vertices])
+    bottomArcs.append(Entity(model=Mesh(vertices=vertices, mode='line'), color=color.orange, scale=1))
+    # increase arc radius
+    arcRadius += 0.1
 
 def rotate_sphere():
     # add sphere rotation; a black rotates on the axis of symmetry
