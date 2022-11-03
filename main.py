7c887b2a2fa3457938bde4bfc641773bec7ced41
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2022-11-03 14:07:23 +1300

    created an arc using lines

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import colorama
 from ursina import *
 import numpy as np
 
@@ -19,6 +18,24 @@ sphere = Entity(model='sphere', color=color.black, scale=sphere_scale, position=
 # square = Entity(model='quad', color=color.red, scale=sphere_scale, position=(0,0,0))
 # create photon ring
 circle = Entity(model='circle', color=color.rgba(243,216,192,100), scale=tuple(i+0.05 for i in sphere_scale), position=(0,0,0))
+# add lines
+# line = Entity(model='line', color=color.orange, scale=(8,8), position=(0,-3))
+# generate vertices
+vertices = []
+for y in np.linspace(0,5):
+    x = np.sqrt(25-(y**2))
+    # positive square root answer
+    vertices.append([x, y, 0])
+
+# add negative square root answers
+
+for i in reversed(vertices.copy()):
+    vertices.append([i[j]*-1 if j==0 else i[j] for j in range(len(i))])
+# vertices.append([[i[j]*-1 if j==0 else i[j] for j in range(len(i))] for i in vertices])
+# vertices[-1] = vertices[-1][0]
+# convert to a tuple
+vertices = tuple([tuple(i) for i in vertices])
+line = Entity(model=Mesh(vertices=vertices, mode='line'), color=color.orange, scale=1)
 
 def rotate_sphere():
     # add sphere rotation; a black rotates on the axis of symmetry
