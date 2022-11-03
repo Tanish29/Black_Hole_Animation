b48d7b0fe1f3fe13f0d38852b9cbb45ab509ecc1
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2022-11-04 12:40:18 +1300

    bottom arcs are non-linearly spaced

--- main.py
+++ main.py
@@ -18,15 +18,17 @@ sphere_scale = tuple(i*radius for i in (1,1,1))
 sphere = Entity(model='sphere', color=color.black, scale=sphere_scale, position=(0,0,0), dz=1e+35)
 # create photon ring
 circle = Entity(model='circle', color=color.rgba(243,216,192,100), scale=tuple(i+0.05 for i in sphere_scale), position=(0,0,0))
+# point = Entity(model='line', color=color.blue, scale = (1,1,1), position=(5,0,0))
 # add bottom arcs/black hole's underside
 bottomArcs = []
-arcRadius = radius
-# create 10 arcs
-for numArcs in range(50):
+arcRadius = radius/1.5
+# create n arcs
+n = 200
+for numArcs in range(n):
     # generates points
     vertices = []
     # loop through a set of possible y values of a circle
-    for y in np.linspace(0, -arcRadius):
+    for y in np.linspace(arcRadius, -arcRadius):
         x = np.sqrt((arcRadius**2) - (y)**2)
         # positive square root answer
         vertices.append([x, y, 0])
@@ -36,8 +38,12 @@ for numArcs in range(50):
     # convert to a tuple
     vertices = tuple([tuple(i) for i in vertices])
     bottomArcs.append(Entity(model=Mesh(vertices=vertices, mode='line'), color=color.orange, scale=1))
-    # increase arc radius
-    arcRadius += 0.1
+    # increase arc radius using a nonlinear spaced vector of numbers
+    if numArcs != round(n/1.1):
+        arcRadius += (n/10000)*(np.linspace(0,1,n)**2)[numArcs]
+    else:
+        # significantly increase arc radius to make it look more like a black hole
+        arcRadius += 0.1
 
 def rotate_sphere():
     # add sphere rotation; a black rotates on the axis of symmetry
