b64102a40b3c7941fa3767c3eee93125869622b6
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2022-11-04 14:28:47 +1300

    added arc rotation and color gradient

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ radius = 5 # scale factor
 sphere_scale = tuple(i*radius for i in (1,1,1))
 sphere = Entity(model='sphere', color=color.black, scale=sphere_scale, position=(0,0,0), dz=1e+35)
 # create photon ring
-circle = Entity(model='circle', color=color.rgba(243,216,192,100), scale=tuple(i+0.05 for i in sphere_scale), position=(0,0,0))
-# point = Entity(model='line', color=color.blue, scale = (1,1,1), position=(5,0,0))
+circle = Entity(model=Circle(resolution=50, radius=radius/2, mode="line", thickness=4),
+                color=color.rgb(255,252,236),
+                position=(0,0,0))
 # add bottom arcs/black hole's underside
 bottomArcs = []
 arcRadius = radius/1.5
@@ -28,7 +29,7 @@ for numArcs in range(n):
     # generates points
     vertices = []
     # loop through a set of possible y values of a circle
-    for y in np.linspace(arcRadius, -arcRadius):
+    for y in np.linspace(arcRadius, -arcRadius, 2000):
         x = np.sqrt((arcRadius**2) - (y)**2)
         # positive square root answer
         vertices.append([x, y, 0])
@@ -37,19 +38,35 @@ for numArcs in range(n):
         vertices.append([i[j]*-1 if j==0 else i[j] for j in range(len(i))])
     # convert to a tuple
     vertices = tuple([tuple(i) for i in vertices])
-    bottomArcs.append(Entity(model=Mesh(vertices=vertices, mode='line'), color=color.orange, scale=1))
+    bottomArcs.append(Entity(model=Mesh(vertices=vertices, mode='line'), color=color.rgba(np.linspace(255,237,n)[numArcs],
+                                                                                          np.linspace(252,130,n)[numArcs],
+                                                                                          np.linspace(236,54,n)[numArcs],
+                                                                                          np.linspace(255,120,n)[numArcs])))
+    # if you want just a circle (not an arc) use code below
+    # bottomArcs.append(Entity(model=Circle(resolution=1000, radius=arcRadius, mode="line", thickness=4),
+    #                                 color=color.rgba(np.linspace(255,237,n)[numArcs],
+    #                                                  np.linspace(252,130,n)[numArcs],
+    #                                                  np.linspace(236,54,n)[numArcs],
+    #                                                  255-numArcs),
+    #                                 position=(0,0,0)))
     # increase arc radius using a nonlinear spaced vector of numbers
     if numArcs != round(n/1.1):
         arcRadius += (n/10000)*(np.linspace(0,1,n)**2)[numArcs]
     else:
         # significantly increase arc radius to make it look more like a black hole
-        arcRadius += 0.1
+        arcRadius += 0.08
 
-def rotate_sphere():
+
+def update():
+    ''' update entities/models
+
+    :return: None
+    '''
     # add sphere rotation; a black rotates on the axis of symmetry
     sphere.rotation_z += sphere.dz
-    # sphere.rotate(value=np.array([1,1,1])*1e+35)
-sphere.update = rotate_sphere
+    # sphere.rotate(value=np.array([1,1,1])*sphere.dz)
+    for bEntity in bottomArcs:
+        bEntity.rotation_z += 1
 
 # make free view available
 EditorCamera()
