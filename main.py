ce1d663962a51a9c787c58c96755ab34f71cab62
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2022-12-27 13:04:57 +1300

    added bottom and partially top acc disk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import ursina
 from ursina import *
+from ursina.scripts import merge_vertices
 import numpy as np
 
 ''' Script below creates an animation of a rotating black hole
@@ -15,9 +17,9 @@ window.color = color.black
 # create a sphere
 radius = 5 # scale factor
 sphere_scale = tuple(i*radius for i in (1,1,1))
-sphere = Entity(model='sphere', color=color.black, scale=sphere_scale, position=(0,0,0), dz=1e+35)
+sphere = Entity(model='sphere', color=color.black, scale=sphere_scale, position=(0,0,0), dz=1e+10)
 # create photon ring
-circle = Entity(model=Circle(resolution=50, radius=radius/2, mode="line", thickness=4),
+PhotonRing = Entity(model=Circle(resolution=50, radius=radius/1.9, mode="line", thickness=2),
                 color=color.rgb(255,252,236),
                 position=(0,0,0))
 # add bottom arcs/black hole's underside
@@ -26,10 +28,45 @@ arcRadius = radius/1.5
 # create n arcs
 n = 200
 for numArcs in range(n):
+    # # generates points
+    # vertices = []
+    # # loop through a set of possible y values of a circle
+    # for y in np.linspace(arcRadius, -arcRadius, 2000):
+    #     x = np.sqrt((arcRadius**2) - (y)**2)
+    #     # positive square root answer
+    #     vertices.append([x, y, 0])
+    # # add negative square root answers
+    # for i in reversed(vertices.copy()):
+    #     vertices.append([i[j]*-1 if j==0 else i[j] for j in range(len(i))])
+    # # convert to a tuple
+    # vertices = tuple([tuple(i) for i in vertices])
+    # bottomArcs.append(Entity(model=Mesh(vertices=vertices, mode='line'), color=color.rgba(np.linspace(255,237,n)[numArcs],
+    #                                                                                       np.linspace(252,130,n)[numArcs],
+    #                                                                                       np.linspace(200,54,n)[numArcs],
+    #                                                                                       255-numArcs)))
+    # if you want just a circle (not an arc) use code below
+    bottomArcs.append(Entity(model=Circle(resolution=1000, radius=arcRadius, mode="line", thickness=2),
+                                    color=color.rgba(np.linspace(255,237,n)[numArcs],
+                                                     np.linspace(252,130,n)[numArcs],
+                                                     np.linspace(236,54,n)[numArcs],
+                                                     255-numArcs),
+                                    position=(0,0,0)))
+    # increase arc radius using a nonlinear spaced vector of numbers
+    if numArcs != round(n/1.1):
+        arcRadius += (n/10000)*(np.linspace(0,1,n)**2)[numArcs]
+    else:
+        # significantly increase arc radius to make it look more like a black hole
+        arcRadius += 0.08
+
+# add accretion disk
+accArcs = []
+arcRadius = radius/1.5
+n = 200
+for numArcs in range(n):
     # generates points
     vertices = []
     # loop through a set of possible y values of a circle
-    for y in np.linspace(arcRadius, -arcRadius, 2000):
+    for y in np.linspace(0, -arcRadius, 1000):
         x = np.sqrt((arcRadius**2) - (y)**2)
         # positive square root answer
         vertices.append([x, y, 0])
@@ -38,24 +75,29 @@ for numArcs in range(n):
         vertices.append([i[j]*-1 if j==0 else i[j] for j in range(len(i))])
     # convert to a tuple
     vertices = tuple([tuple(i) for i in vertices])
-    bottomArcs.append(Entity(model=Mesh(vertices=vertices, mode='line'), color=color.rgba(np.linspace(255,237,n)[numArcs],
+    accArcs.append(Entity(model=Mesh(vertices=vertices, mode='line', thickness=2), color=color.rgba(np.linspace(255,237,n)[numArcs],
                                                                                           np.linspace(252,130,n)[numArcs],
-                                                                                          np.linspace(236,54,n)[numArcs],
-                                                                                          np.linspace(255,120,n)[numArcs])))
-    # if you want just a circle (not an arc) use code below
-    # bottomArcs.append(Entity(model=Circle(resolution=1000, radius=arcRadius, mode="line", thickness=4),
-    #                                 color=color.rgba(np.linspace(255,237,n)[numArcs],
-    #                                                  np.linspace(252,130,n)[numArcs],
-    #                                                  np.linspace(236,54,n)[numArcs],
-    #                                                  255-numArcs),
-    #                                 position=(0,0,0)))
+                                                                                          np.linspace(200,54,n)[numArcs],
+                                                                                          255-numArcs),
+                          rotation_x=90))
+
+    accArcs.append(Entity(parent=accArcs[-1], model=Mesh(vertices=vertices, mode='line', thickness=2),
+                                         color=color.rgba(np.linspace(255,237,n)[numArcs],
+                                                                                          np.linspace(252,130,n)[numArcs],
+                                                                                          np.linspace(200,54,n)[numArcs],
+                                                                                          255-numArcs),
+                          rotation_x=90))
+    hitinfo = accArcs[-1].intersects()
+    if hitinfo.hit:
+        accArcs[-1].combine()
+        print("top arc intersects")
+
     # increase arc radius using a nonlinear spaced vector of numbers
-    if numArcs != round(n/1.1):
-        arcRadius += (n/10000)*(np.linspace(0,1,n)**2)[numArcs]
+    if numArcs != round(n / 1.1):
+        arcRadius += (n / 5000) * (np.linspace(0, 1, n) ** 2)[numArcs]
     else:
         # significantly increase arc radius to make it look more like a black hole
         arcRadius += 0.08
-
 
 def update():
     ''' update entities/models
@@ -67,6 +109,8 @@ def update():
     # sphere.rotate(value=np.array([1,1,1])*sphere.dz)
     for bEntity in bottomArcs:
         bEntity.rotation_z += 1
+    # for AEntity in accArcs:
+    #     AEntity.rotation_z += 1
 
 # make free view available
 EditorCamera()
