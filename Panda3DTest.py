bd874ca8ff5fcce9354bb8a5d22be3a9c50ba0cc
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2023-01-03 14:53:44 +1300

    creating sphere vertices and normals using slices/slacks

--- Panda3DTest.py
+++ Panda3DTest.py
@@ -12,24 +12,34 @@ class MyGame(ShowBase):
         super().__init__()
 
         # create a GeomVertexData object
-        vertex_data = GeomVertexData('sphere', GeomVertexFormat().getV3c4(), Geom.UHDynamic)
+        vertex_data = GeomVertexData('sphere', GeomVertexFormat().getV3n3c4(), Geom.UHDynamic)
 
         # create Geom Vertex Writers
         vertex_writer = GeomVertexWriter(vertex_data, 'vertex')
+        normal_writer = GeomVertexWriter(vertex_data, 'normal')
         color_writer = GeomVertexWriter(vertex_data, 'color')
 
         # set sphere properties
         radius = 5
+        slices = 16
+        stacks = 16
 
-        # generate vertices, cartesian equation for a sphere - x^2 + y^2 + z^2 = R^2
-        for x in np.arange(0, radius):
-            print('x=',x)
-            y = np.sqrt(radius**2-x**2)
-            print('y=',y)
-            z = np.sqrt(radius**2-x**2-y**2)
-            print('z=',z)
-            # write data
-            vertex_writer.addData3f(x,y,z)
+        # generate vertices based on the above properties
+        for i in range(stacks+1):
+            # calculate angle in the y-z plane for a full circle
+            phi = np.pi * (i/stacks)
+            # calculate z cartesian coordinate
+            z = radius * np.sin(phi)
+            for j in range(slices+1):
+                # calculate angle in the x-y plane
+                theta = np.pi * (j/slices)
+                # calculate x and y, remember rcos(theta) is the stack radius
+                x = (radius*np.cos(phi))*np.cos(theta)
+                y = (radius*np.cos(phi))*np.sin(theta)
+                # add vertiex and normal
+                print('x,y,z=',x,y,z)
+                vertex_writer.addData3f(x,y,z)
+                normal_writer.addData3f(x,y,z)
 
         # set sphere color
         color_writer.addData4f(1,0,0,1)
@@ -44,7 +54,6 @@ class MyGame(ShowBase):
         # create a node path and put it in scene graph
         sphereNodePath = NodePath(sphereNode)
         sphereNodePath.reparentTo(self.render)
-
 
 
 inst1 = MyGame()
