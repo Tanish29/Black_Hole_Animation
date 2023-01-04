3d1795464a1625c4a607d93bf58c50f203887fb8
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2023-01-04 13:50:57 +1300

    a black sphere/BH has been added

--- Panda3DTest.py
+++ Panda3DTest.py
@@ -2,6 +2,7 @@ from panda3d.core import loadPrcFile
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import GeomVertexFormat, GeomVertexData
 from panda3d.core import Geom, GeomTriangles, GeomVertexWriter, GeomNode, NodePath
+from panda3d.core import Vec3
 import numpy as np
 
 # load config file
@@ -12,7 +13,7 @@ class MyGame(ShowBase):
         super().__init__()
 
         # create a GeomVertexData object
-        vertex_data = GeomVertexData('sphere', GeomVertexFormat().getV3n3c4(), Geom.UHDynamic)
+        vertex_data = GeomVertexData('sphere', GeomVertexFormat().getV3n3c4(), Geom.UHStatic)
 
         # create Geom Vertex Writers
         vertex_writer = GeomVertexWriter(vertex_data, 'vertex')
@@ -21,39 +22,60 @@ class MyGame(ShowBase):
 
         # set sphere properties
         radius = 5
-        slices = 16
-        stacks = 16
+        slices = 50
+        stacks = 50
 
         # generate vertices based on the above properties
         for i in range(stacks+1):
             # calculate angle in the y-z plane for a full circle
-            phi = np.pi * (i/stacks)
+            phi = (2*np.pi) * (i/stacks)
             # calculate z cartesian coordinate
             z = radius * np.sin(phi)
             for j in range(slices+1):
                 # calculate angle in the x-y plane
-                theta = np.pi * (j/slices)
+                theta = (2*np.pi) * (j/slices)
                 # calculate x and y, remember rcos(theta) is the stack radius
                 x = (radius*np.cos(phi))*np.cos(theta)
                 y = (radius*np.cos(phi))*np.sin(theta)
-                # add vertiex and normal
-                print('x,y,z=',x,y,z)
+                # add vertex and normal
                 vertex_writer.addData3f(x,y,z)
                 normal_writer.addData3f(x,y,z)
+                # set sphere color
+                color_writer.addData4f(0,0,0,1)
 
-        # set sphere color
-        color_writer.addData4f(1,0,0,1)
+        # Create the triangles
+        triangles = GeomTriangles(Geom.UHStatic)
+        for i in range(stacks):
+            for j in range(slices):
+                # stack index
+                K1 = i*(slices+1) + j
+                # next stack index
+                K2 = (i+1)*(slices+1) + j
+                # next slice index
+                K1_1 = i*(slices+1) + (j+1)
+                # next slice and stack index
+                K2_1 = (i+1)*(slices+1) + (j+1)
+                triangles.addVertices(K1,K2,K1_1)
+                triangles.addVertices(K1_1,K2,K2_1)
+
 
         # create a geom object
         sphereGeom = Geom(vertex_data)
+        sphereGeom.addPrimitive(triangles)
 
-        # create a geomNode
+        # create a geom node
         sphereNode = GeomNode('sphere')
         sphereNode.addGeom(sphereGeom)
 
         # create a node path and put it in scene graph
         sphereNodePath = NodePath(sphereNode)
+        sphereNodePath.setPos(0,30,0)
         sphereNodePath.reparentTo(self.render)
+
+        # load a model
+        box = self.loader.loadModel("models/box")
+        box.setPos(0,10,0)
+        box.reparentTo(self.render)
 
 
 inst1 = MyGame()
