1b8f8fd126bdeb1588914a0dd5592ab25f27c131
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2023-01-06 16:22:27 +1300

    added photon ring to black hole

--- Panda3DTest.py
+++ Panda3DTest.py
@@ -1,8 +1,8 @@
 from panda3d.core import loadPrcFile
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import GeomVertexFormat, GeomVertexData
-from panda3d.core import Geom, GeomTriangles, GeomVertexWriter, GeomNode, NodePath
-from panda3d.core import Vec3
+from panda3d.core import GeomVertexFormat, GeomVertexData, GeomVertexWriter, \
+                         GeomTriangles, Geom, GeomNode, GeomLinestrips
+from panda3d.core import NodePath
 import numpy as np
 
 # load config file
@@ -12,13 +12,13 @@ class MyGame(ShowBase):
     def __init__(self):
         super().__init__()
 
+    def createBlackHole(self):
         # create a GeomVertexData object
-        vertex_data = GeomVertexData('sphere', GeomVertexFormat().getV3n3c4(), Geom.UHStatic)
+        vertex_data = GeomVertexData('sphere', GeomVertexFormat().getV3n3c4(), Geom.UHDynamic)
 
         # create Geom Vertex Writers
         vertex_writer = GeomVertexWriter(vertex_data, 'vertex')
         normal_writer = GeomVertexWriter(vertex_data, 'normal')
-        color_writer = GeomVertexWriter(vertex_data, 'color')
 
         # set sphere properties
         radius = 5
@@ -39,9 +39,7 @@ class MyGame(ShowBase):
                 y = (radius*np.cos(phi))*np.sin(theta)
                 # add vertex and normal
                 vertex_writer.addData3f(x,y,z)
-                normal_writer.addData3f(x,y,z)
-                # set sphere color
-                color_writer.addData4f(0,0,0,1)
+                # normal_writer.addData3f(x,y,z)
 
         # Create the triangles
         triangles = GeomTriangles(Geom.UHStatic)
@@ -58,7 +56,6 @@ class MyGame(ShowBase):
                 triangles.addVertices(K1,K2,K1_1)
                 triangles.addVertices(K1_1,K2,K2_1)
 
-
         # create a geom object
         sphereGeom = Geom(vertex_data)
         sphereGeom.addPrimitive(triangles)
@@ -70,13 +67,65 @@ class MyGame(ShowBase):
         # create a node path and put it in scene graph
         sphereNodePath = NodePath(sphereNode)
         sphereNodePath.setPos(0,30,0)
+        sphereNodePath.setColor(r=0, g=0, b=0, a=1)
         sphereNodePath.reparentTo(self.render)
 
+    def createPhotonRing(self):
+        vertex_data = GeomVertexData('circle', GeomVertexFormat().getV3c4(), Geom.UHDynamic)
+        vertex_writer = GeomVertexWriter(vertex_data, 'vertex')
+
+        # circle properties
+        radius = 5.2
+
+        # generate circle vertices
+        x_vals = np.linspace(-radius, radius, 500)
+        neg_z_vals = {}
+        # add positive z values for each x value
+        for x in x_vals:
+            z = np.sqrt(radius**2 - x**2)
+            # write point to data
+            vertex_writer.addData3f(x,0,z)
+            neg_z_vals[x] = -z
+        # add negative z values for each x value
+        for x in reversed(x_vals):
+            vertex_writer.addData3f(x,0,neg_z_vals[x])
+
+        # create a geom line strip object
+        circleLines = GeomLinestrips(Geom.UHDynamic)
+        # add all vertices
+        circleLines.addConsecutiveVertices(start=0, num_vertices=vertex_data.getNumRows())
+        # close line connection
+        circleLines.closePrimitive()
+
+        # create a geom object
+        circleGeom = Geom(vertex_data)
+        circleGeom.addPrimitive(circleLines)
+
+        # create a node to store geom object
+        circleNode = GeomNode('circle')
+        circleNode.addGeom(circleGeom)
+
+        # create a node path to store geom node
+        circleNodePath = NodePath(circleNode)
+        circleNodePath.setPos(0,30,0)
+        circleNodePath.setColor(r=1, g=0.64, b=0, a=1)
+        circleNodePath.setRenderModeThickness(4)
+        circleNodePath.reparentTo(self.render)
+
+    def createParticleSystem(self):
+        pass
+
+    def setBackGroundColor(self):
+        # set background color
+        self.setBackgroundColor(0,0,0)
+
+    def loadModel(self):
         # load a model
         box = self.loader.loadModel("models/box")
         box.setPos(0,10,0)
         box.reparentTo(self.render)
 
-
 inst1 = MyGame()
+inst1.createBlackHole()
+inst1.createPhotonRing()
 inst1.run()
